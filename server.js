aae0316cdc393048b91c8d6598521f4a43755132
bonotan_k <bonotan_k@ca01.ub.lan>
2026-03-18 15:26:30 +0800

    MoongoDB-Favorite-Pokemon

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-
+// Ensure your .env file has MONGODB_URI
 mongoose.connect(process.env.MONGODB_URI);
 
 const Pokemon = mongoose.model('pokemon', new mongoose.Schema({
@@ -23,4 +23,9 @@ app.post('/api/pokemon', async (req, res) => {
     res.send(pokemon);
 });
 
+app.get('/api/pokemon', async (req, res) => {
+    const allPokemon = await Pokemon.find();
+    res.send(allPokemon);
+});
 
+app.listen(3000, () => console.log('Server running on port 3000'));
